4cc46d3bc34b66b8247b083c2fb721650aaf7393
d7oomy661122 <d7oomy661122@gmail.com>
2026-03-08 05:55:34 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -242,3 +242,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+window.addEventListener("load", function(){
+
+    const preloader = document.getElementById("preloader");
+
+    if(preloader){
+        preloader.style.display = "none";
+    }
+
+});
